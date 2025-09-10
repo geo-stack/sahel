0bb8e3b136d2b9a611f4df73a4188b790ace158e
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2025-09-10 16:14:52 -0400

    Create __init__.py

---
+++ sahel/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Copyright (C) Les solutions géostack, Inc
+#
+# This file was produced as part of a research project conducted for
+# The World Bank Group and is licensed under the terms of the MIT license.
+#
+# For inquiries, contact: info@geostack.ca
+# Repository: https://github.com/geo-stack/sahel
+# =============================================================================
+
+import os.path as osp
+__datadir__ = osp.join(osp.dirname(osp.dirname(__file__)), 'data', 'data')
