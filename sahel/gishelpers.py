ca048afc8b1b71d347d87b0aeaa28a3feb8053e9
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2025-10-10 10:13:53 -0400

    Export SWB data from zip file to tiff (in addition to HGT)

--- sahel/gishelpers.py
+++ sahel/gishelpers.py
@@ -63,23 +63,36 @@ def convert_hgt_to_geotiff(
     """
     # Find the .hgt file name inside the zip archive.
     with zipfile.ZipFile(zip_path, 'r') as zf:
+        hgt_filename = None
+        swb_filename = None
         for fname in zf.namelist():
             if fname.lower().endswith('.hgt'):
                 hgt_filename = fname
-                break
-        else:
-            raise FileNotFoundError(f"No .hgt file found in '{zip_path}'.")
+            elif fname.lower().endswith('.swb'):
+                swb_filename = fname
 
-    # Open the .hgt file using rasterio (via GDAL VFS).
+        if hgt_filename is None:
+            raise FileNotFoundError(f"No .hgt file found in '{zip_path}'.")
+        if swb_filename is None:
+            raise FileNotFoundError(f"No .swt file found in '{zip_path}'.")
+
+    # Open the .hgt and .swb file using rasterio (via GDAL VFS).
     vsi_path = f'/vsizip/{zip_path}/{hgt_filename}'
     with rasterio.open(vsi_path) as src:
         profile = src.profile
-        data = src.read(1)
-        profile.update(driver='GTiff')
+        hgt_data = src.read(1)
+
+    vsi_path = f'/vsizip/{zip_path}/{swb_filename}'
+    with rasterio.open(vsi_path) as src:
+        swt_data = src.read(1).astype(hgt_data.dtype)
+
 
     # Write to GeoTIFF.
+    profile.update(driver='GTiff', count=2, dtype=hgt_data.dtype)
     if compress is not None:
         profile.update(compress=compress)
 
     with rasterio.open(tif_path, 'w', **profile) as dst:
-        dst.write(data, 1)
+        dst.write(hgt_data, 1)
+        dst.write(swt_data, 2)
+
