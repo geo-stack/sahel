332d10a4976c3c1ec3bf874ac789556f34462e74
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2025-11-13 15:54:38 -0500

    Create study area bbox

--- create_study_area_geom.py
+++ create_study_area_geom.py
@@ -9,22 +9,43 @@
 # Repository: https://github.com/geo-stack/sahel
 # =============================================================================
 
-# ---- Standard imports.
+# ---- Standard imports
 from pathlib import Path
+from shapely.geometry import box
 
-# ---- Local imports.
+# ---- Third party imports
+import geopandas as gpd
+
+# ---- Local imports
 from sahel import __datadir__
 from sahel.geometry import buffer_geometry, create_unified_geometry
 
 dst_crs = 'ESRI:102022'    # Africa Albers Equal Area Conic
 buffer_dist = 100 * 10**3  # in meters
 
-boundary_path = Path(__datadir__) / 'gadm' / 'unified_boundary.json'
-if not boundary_path.exists():
-    create_unified_geometry(boundary_path, dst_crs)
+outdir = Path(__datadir__) / 'gadm'
 
-buff_geo_path = (
-    boundary_path.parent / f'buffered_boundary_{int(buffer_dist/1000)}km.json'
+# Create study area from Global Administrative Area (GADM) from the
+# target countries.
+study_area_path = outdir / 'study_area.json'
+if not study_area_path.exists():
+    create_unified_geometry(study_area_path, dst_crs)
+
+# Add a buffer (in meters).
+buff_study_area_path = (
+    outdir / f'study_area_({int(buffer_dist/1000)}km buffer).json'
     )
-if not buff_geo_path.exists():
-    buffer_geometry(boundary_path, buff_geo_path, buffer_dist)
+if not buff_study_area_path.exists():
+    buffered_gdf = buffer_geometry(
+        study_area_path, buff_study_area_path, buffer_dist
+        )
+
+# Create bounding box of the buffered study area.
+study_area_bbox_geo_path = (
+    outdir / f'study_area_bbox_({int(buffer_dist/1000)}km buffer).json'
+    )
+if not study_area_bbox_geo_path.exists():
+    minx, miny, maxx, maxy = buffered_gdf.total_bounds
+    bbox_polygon = box(minx, miny, maxx, maxy)
+    bbox_gdf = gpd.GeoDataFrame({'geometry': [bbox_polygon]}, crs=dst_crs)
+    bbox_gdf.to_file(study_area_bbox_geo_path, driver='GeoJSON')
