fd4e48f6b8c9a9942733630071d65ab15e00787d
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2025-12-05 14:34:03 -0500

    Update process_hydro_basins.py

--- process_hydro_basins.py
+++ process_hydro_basins.py
@@ -10,54 +10,40 @@
 # =============================================================================
 
 # ---- Standard imports
-from pathlib import Path
 
 # ---- Third party imports
-import numpy as np
 import geopandas as gpd
+from shapely.geometry import box
 
 # ---- Local imports
-from sahel import __datadir__
+from hdml import __datadir__ as datadir
 
 
-study_area_gdf = gpd.read_file(
-    Path(__datadir__) / 'gadm' / 'buffered_boundary_100km.json'
-    )
+africa_gdf = gpd.read_file(datadir / 'coastline' / 'africa_landmass.gpkg')
+africa_shape = africa_gdf.union_all()
 
-basins_all_path = Path(__datadir__) / 'hydro_atlas' / 'BasinATLAS_v10.gdb'
-basins_path = basins_all_path.parent / 'sahel_BasinATLAS_v10.gpkg'
-if not basins_path.exists():
-    for layer in range(1, 13):
-        layer_name = f'BasinATLAS_v10_lev{layer:02d}'
-        print(f"Clipping layer {layer:02d}...", flush=True)
-        basins_gdf = gpd.read_file(str(basins_all_path), layer=layer_name)
-        basins_gdf = basins_gdf.to_crs('ESRI:102022')
+minx, miny, maxx, maxy = africa_gdf.total_bounds
+africa_bbox = box(minx, miny, maxx, maxy)
 
-        basins_clipped = gpd.clip(basins_gdf, study_area_gdf)
-        basins_clipped.to_file(basins_path, layer=layer_name, driver="GPKG")
+basins_all_path = datadir / 'hydro_atlas' / 'BasinATLAS_v10.gdb'
 
-# %%
+level = 12
 
-for layer in range(1, 13):
-    layer_name = f'BasinATLAS_v10_lev{layer:02d}'
-    basins_gdf = gpd.read_file(basins_path, layer=layer_name)
+layer_name = f'BasinATLAS_v10_lev{level:02d}'
 
-    area_m2 = basins_gdf.geometry.area
-    area_km2 = area_m2 / 1e6
-    med_km2 = np.median(area_km2)
+print(f'Reading {layer_name} from {basins_all_path.name}...', flush=True)
+basins_gdf = gpd.read_file(str(basins_all_path), layer=layer_name)
+print('Number of basins:', len(basins_gdf), flush=True)
 
-    print(layer_name)
-    print(f'p50: {med_km2} km2')
-    print('ncels:', int(med_km2 / 0.250**2))
+print('Projecting to ESRI:102022...', flush=True)
+basins_gdf = basins_gdf.to_crs('ESRI:102022')
 
-#  2200 cells -> match basins level 10 to 12 → ~137.5 km²
-#  3215 cells -> match basins level 9 → ~200.9 km²
-#  7553 cells -> match basins level 8 → ~472.1 km²
-# 23684 cells -> match basins level 7 → ~1480.3 km²
-# 70938 cells -> match basins level 6 → ~4433.6 km²
+# We only clip to the bounding box of the African continent because
+# clipping to the shape is too long.
+print("Clipping to African continent bbox...", flush=True)
+basins_gdf = gpd.clip(basins_gdf, africa_bbox)
+print('Number of basins:', len(basins_gdf), flush=True)
 
-# At 250 m resolution, even the "small" basins are regional in scale
-# (tens to thousands of km²). That means using those median-based thresholds
-# in flow-accumulation stream extraction will keep only fairly large rivers
-# and main valley systems, which is exactly what we want when modelling
-# regional groundwater / water-table depth (WTD) rather than local drains.
+print("Saving restuls to file...", flush=True)
+basins_path = basins_all_path.parent / f'basins_lvl{level:02d}_102022.gpkg'
+basins_gdf.to_file(basins_path, layer=layer_name, driver="GPKG")
